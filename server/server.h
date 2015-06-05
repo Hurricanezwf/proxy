9583a4e0563d50027b2c47d29ca84174570c0e4d
Hurricanezwf <1094646850.qq.com>
2015-06-06 00:22:47 +0800

    添加进程记录连接功能

--- server/server.h
+++ server/server.h
@@ -11,11 +11,12 @@
 #include <arpa/inet.h>
 #include <sys/wait.h>
 #include <pthread.h>
+#include <list>
 #include <set>
-#include <vector>
 #include <sys/mman.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <sys/epoll.h>
 #include "base.h"
 #include "log.h"
 
@@ -23,14 +24,15 @@
 
 #define		SERVER_IP		"192.168.152.128"
 #define		LISTEN_PORT		9000
-#define 	MAX_BACKLOG		100
+#define 	MAX_BACKLOG		(MAX_CHILD_NUM * MAX_EPOLL_SIZE)
 
 #define     SHM_NAME        "/shm_zwf"
 
 #define     MAX_CHILD_NUM   2
+#define     MAX_EPOLL_SIZE  15 
 
 typedef void (*TResourceClear)();
-
+typedef void (*ChildClear)(std::list<SOCK_FD> *pList);
 
 typedef struct {
     pid_t   pid;                //所属进程号
@@ -56,19 +58,20 @@ typedef struct {
  * Note:        无
  */
 inline static void CHK(BOOL bSuccess, s8 *pchMsg, TResourceClear func);
+inline static void CHK2(BOOL bSuccess, s8 *pchMsg, void (*func)(void*), void *param);
 
 
 
-
-
+static void server_init();
 static SOCK_FD Setup();
 static void Destroy();
-static void ChildTerminate(int nSigNo);                             //SIGCHLD信号处理
-static void ChildProcessHandle();                                   //子进程逻辑处理
-static void CloseConnectedFDs(std::set<SOCK_FD> &setFDs);           //关闭所有的已连接套接字
-static void CreateShareMemory();                                    //创建共享内存
-static void InitChild(u8 byIndex);                                  //初始化指定的子进程
-
+static void ChildTerminate(int nSigNo);                                         //SIGCHLD信号处理
+static void ChildProcessHandle(u8 byIndex);                                               //子进程逻辑处理
+static void ChildGabageClear(void *pList);                                      //子进程资源清理
+static void CloseConnectedFDs(std::list<SOCK_FD> *pclientList);                 //关闭所有的已连接套接字
+static void CreateShareMemory();                                                //创建共享内存
+static void InitChild(u8 byIndex);                                              //初始化指定的子进程
+static s32  FindClientFromList(std::list<SOCK_FD> &clientList, SOCK_FD fd);     //从list中查找制定的fd
 
 
 //调试信息打印
